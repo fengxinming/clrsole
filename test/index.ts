5417629bdbbba9fe742c513d29d4074fec6ddcc7
Jesse.Feng <fxm0016@126.com>
2025-03-22 11:27:31 +0800

    feat(5.0.0): 代码优化

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,24 @@
-import { clrsole, getLogger } from '../dist/index.mjs';
+import { clrsole, getLogger } from '../src';
 
-const appLogger = getLogger('app', { level: 'trace' });
+const appLogger = getLogger('app', {
+  level: 'TRACE',
+  depth: 2
+});
+
 appLogger.trace('message', null, undefined, { message: 'message2' }, new Error('error'));
-appLogger.debug('message', 'message2');
+appLogger.debug('message', 'message2', {
+  a: 1,
+  b:
+   {
+     c: 1,
+     d: {
+       e: 1,
+       f: {
+         g: 1
+       }
+     }
+   }
+});
 // eslint-disable-next-line no-undef
 appLogger.info(NaN, 0, BigInt('9007199254740991'), Infinity);
 appLogger.warn('message', 'message2');
@@ -10,3 +26,6 @@ appLogger.error(true, false);
 appLogger.fatal('message', Symbol('message2'));
 
 clrsole.red('red message');
+clrsole.green('green message');
+
+process.exit(0);
